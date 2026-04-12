49131eb19e03b61348026bb48c829c3d296e2a2f
ZatharV <atharvmore2622@gmail.com>
2026-04-12 00:46:26 -0600

    IOCTL issue complete

--- base_external/package/bird-classifier/bird-classifier.mk
+++ base_external/package/bird-classifier/bird-classifier.mk
@@ -5,7 +5,7 @@
 #
 ##############################################################
 
-BIRD_CLASSIFIER_VERSION = main
+BIRD_CLASSIFIER_VERSION = 07fc7a1f9f2ec9f65deef63fcba4fb9224c45819
 BIRD_CLASSIFIER_SITE = https://github.com/cu-ecen-aeld/final-project-shrutikalyankar.git
 BIRD_CLASSIFIER_SITE_METHOD = git
 
